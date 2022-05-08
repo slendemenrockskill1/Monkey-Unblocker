1cf61a4e65b6057feb8e7e5baaacdcf20e9877f1
illusions <97645434+illusionTBA@users.noreply.github.com>
2022-05-08 12:38:56 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,4 +1,3 @@
-const form = document.querySelector('form');
 const input = document.querySelector('.galaxy-input');
 const searchBTN = document.querySelector('.search-btn');
 
@@ -36,22 +35,6 @@ searchBTN.addEventListener('click', async event => {
 
 });
 
-
-
-
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-
-
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    });
-});
 
 function isUrl(val = '') {
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
