92bcb6959d86527b8347344480c0be36855849dc
illusionTBA <illusionztba@gmail.com>
2022-06-24 13:17:34 -0400

    Fix, Remove blackList

--- static/index.js
+++ static/index.js
@@ -12,7 +12,7 @@ searchBTN.addEventListener('click', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        if (!isUrl(url)) url = 'https://search.brave.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
         var urle = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
         if (urle) {
